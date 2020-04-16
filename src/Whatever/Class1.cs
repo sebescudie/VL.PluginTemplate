bcaff2dc8f6b5afe2e8563baa9d4ea4ffb713de3
sebescudie <seb.espm@gmail.com>
2020-04-16 15:05:58 +0200

    added vs solution

---
+++ src/Whatever/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Whatever
+{
+    public class Foo
+    {
+        public static string Bar(string input)
+        {
+            return input +" Bar";
+        }
+    }
+}
